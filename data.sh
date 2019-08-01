81beb89c46cef8c9600a5af7398886028e1336f8
Scott Little <k1206150@students.katyisd.org>
2019-08-02 00:56:47 +0100

    .

---
+++ data.sh
@@ -0,0 +1,8 @@
+echo "\n License \n"
+cat LICENSE
+echo "\n Readme \n"
+cat README.md
+echo "\n Security Policy \n"
+cat SECURITY.md
+echo "\n Version Info \n"
+cat VERSION.md
---
+++ prepare.sh
@@ -0,0 +1,4 @@
+chmod +x install.sh
+chmod +x aliases.sh
+chmod +x update.sh
+chmod +x data.sh
